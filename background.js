b5e938e4c4cd1db6bff6d2d8796f7d3c66a0e7a2
yuzalsif <liteit68@gmail.com>
2023-10-31 12:25:01 +0300

    feat: Add functionality to manage main and background tabs

--- background.js
+++ background.js
@@ -1,37 +1,69 @@
-// Create an array to store tab names with music playback
-const tabsWithMusic = [];
+// Listen for a message from the popup script
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message === 'set_main_tab') {
+      // Save the main tab ID
+      chrome.storage.local.set({mainTab: request.tabId});
+  } else if (request.message === 'set_background_tab') {
+      // Save the background tab ID
+      chrome.storage.local.set({backgroundTab: request.tabId});
+  }
+});
 
-// Create an AudioContext
-const audioContext = new (chrome.window.AudioContext || chrome.window.webkitAudioContext)();
+// Monitor the audio output of the main tab
+function monitorMainTabAudio() {
+  chrome.storage.local.get(['mainTab', 'backgroundTab'], function(result) {
+    let mainTabId = result.mainTab;
+    let backgroundTabId = result.backgroundTab;
 
-// Create an analyser node to process audio data
-const analyser = audioContext.createAnalyser();
+    // Create an AudioContext
+    let audioContext = new AudioContext();
 
-// Function to update the list of tabs with music
-function updateTabsWithMusic() {
-  chrome.tabs.query({}, (tabs) => {
-    tabsWithMusic.length = 0; // Clear the existing list
+    // Create a MediaStreamSource from the main tab's audio output
+    chrome.tabCapture.capture({audio: true, video: false, audioConstraints: {mandatory: {chromeMediaSource: 'tab', chromeMediaSourceId: mainTabId}}}, function(stream) {
+      let source = audioContext.createMediaStreamSource(stream);
 
-    tabs.forEach((tab) => {
-      if (tab.audible) {
-        tabsWithMusic.push(tab.title);
-      }
+      // Create a GainNode for the background tab's audio output
+      let gainNode = audioContext.createGain();
+
+      // Connect the source to the gainNode
+      source.connect(gainNode);
+
+      // Create a MediaStreamDestination for the background tab's audio output
+      let destination = audioContext.createMediaStreamDestination();
+
+      // Create a MediaRecorder to record the background tab's audio output
+      let recorder = new MediaRecorder(destination.stream);
+
+      // Connect the gainNode to the destination
+      gainNode.connect(destination);
+
+      // Start the recorder
+      recorder.start();
+
+      // Listen for messages from the popup script
+      chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+        if (request.message === 'set_main_tab') {
+          // Save the main tab ID
+          chrome.storage.local.set({mainTab: request.tabId});
+        } else if (request.message === 'set_background_tab') {
+          // Save the background tab ID
+          chrome.storage.local.set({backgroundTab: request.tabId});
+
+          // Create a MediaStreamSource from the background tab's audio output
+          chrome.tabCapture.capture({audio: true, video: false, audioConstraints: {mandatory: {chromeMediaSource: 'tab', chromeMediaSourceId: backgroundTabId}}}, function(stream) {
+            let backgroundSource = audioContext.createMediaStreamSource(stream);
+
+            // Connect the backgroundSource to the gainNode
+            backgroundSource.connect(gainNode);
+          });
+        } else if (request.message === 'adjust_volume') {
+          // Adjust the volume of the background tab's audio output
+          gainNode.gain.value = request.volume;
+        }
+      });
     });
-
-    console.log('Tabs with music:', tabsWithMusic.join(', '));
   });
 }
 
-// Set up audio analysis logic
-navigator.mediaDevices.getUserMedia({ audio: true })
-  .then((stream) => {
-    const audioSource = audioContext.createMediaStreamSource(stream);
-    audioSource.connect(analyser);
-
-    // Schedule the updateTabsWithMusic function to run periodically
-    setInterval(updateTabsWithMusic, 5000); // Every 5 seconds
-  })
-  .catch((error) => {
-    console.error('Error accessing audio:', error);
-  });
-
+// Call the monitor function every second
+setInterval(monitorMainTabAudio, 1000);
